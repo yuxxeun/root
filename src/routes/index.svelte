e67e53a01e583cfc616ee634260b659d8e6f6a52
yuxxeun <itsyuxxeun@gmail.com>
2022-03-24 01:34:38 +0700

    gabut

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,56 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import '../app.css';
+</script>
+
+<svelte:head>
+	<meta name="description" content="thoughts, stories and ideas i guess" />
+	<!-- twitter card things -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content="krido" />
+	<meta name="twitter:description" content="thoughts, stories and ideas i guess" />
+	<meta name="twitter:site" content="@yuxxeun" />
+	<meta name="twitter:creator" content="@yuxxeun" />
+	<meta
+		name="twitter:image"
+		content="https://raw.githubusercontent.com/yuxxeun/yuxxeun.github.io/main/public/gradient.jpg"
+	/>
+	<title>krido</title>
+</svelte:head>
+
+<div class="container">
+	<div class="row col-sm-12 my-5 py-5">
+		<div class="col-md-1" />
+		<div class="col-md-4 text-center" data-aos="fade-up-right">
+			<img
+				src="/profile.jpeg"
+				alt="Avatar"
+				class="img-responsive center-block d-block mx-auto my-4 __avatar"
+				style="width: 19rem;"
+			/>
+		</div>
+		<div class="col-md-6" data-aos="fade-up-left">
+			<h1 class="mt-4 __headline">Look mom, @yuxxeun on metaverse.</h1>
+			<p>I explain with words and code (sometimes).</p>
+			<div class="my-4">
+				<a href="https://github.com/yuxxeun"
+					><i href="https://github.com/yuxxeun" class="__icon lni lni-github lni-32" /></a
+				>
+				<a rel="me" href="https://twitter.com/yuxxeun"
+					><i class="__icon lni lni-twitter lni-32" /></a
+				>
+				<a href="https://pixelfed.social/krido"><i class="__icon lni lni-instagram lni-32" /></a>
+				<a href="https://sasaw.vercel.app/"><i class="__icon lni lni-book lni-32" /></a>
+				<a href="https://poly.work/yuxxeun"><i class="__icon lni lni-sprout lni-32" /></a>
+			</div>
+			<p>
+				You can stop reading in here, but if you want more, I'll give you extra details on my <a
+					href="https://yuxxeun.vercel.app"
+					class="__story">story</a
+				>
+				- its basically a place where i can be me.
+				<br /><br />
+				<i>Thank you for coming and drink your water now!</i>
+			</p>
+		</div>
+	</div>
+</div>
