0ce20025db5ca7ab4b2c0d1811f8a2cd92b1266f
yuxxeun <itsyuxxeun@gmail.com>
2022-07-06 01:17:10 +0700

    url: change edgy library url

--- src/components/desc.svelte
+++ src/components/desc.svelte
@@ -38,7 +38,7 @@
 			<a title="pixelfed.social/krido" href="https://pixelfed.social/krido" target="_blank"
 				><i class="hover:text-white lni lni-gallery lni-32" /></a
 			>
-			<a title="yuxxeun edgy library" href="/books" target="_blank"
+			<a title="yuxxeun edgy library" href="https://sasaw.now.sh" target="_blank"
 				><i class="hover:text-white lni lni-ticket-alt lni-32" /></a
 			>
 			<a title="journey" href="https://poly.work/yuxxeun" target="_blank"
