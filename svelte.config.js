7dd4fcee76a4d0d1fbf81b62545564d0ada8e49a
yuxxeun <itsyuxxeun@gmail.com>
2022-07-10 14:07:38 +0700

    styles: re-design

--- svelte.config.js
+++ svelte.config.js
@@ -2,9 +2,23 @@ import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
-};
+  kit: {
+    // default options are shown
+    adapter: vercel({
+      // if true, will deploy the app using edge functions
+      // (https://vercel.com/docs/concepts/functions/edge-functions)
+      // rather than serverless functions
+      edge: false,
+
+      // an array of dependencies that esbuild should treat
+      // as external when bundling functions
+      external: [],
+
+      // if true, will split your app into multiple functions
+      // instead of creating a single one for the entire app
+      split: false
+    })
+  }
+}
 
 export default config;
