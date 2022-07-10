e64708f4dce7edfd2461d13cbb8cce5c8170cebc
yuxxeun <itsyuxxeun@gmail.com>
2022-07-10 11:47:10 +0700

    deps: vite

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,20 +1,50 @@
 <script>
 	import Desc from '../components/desc.svelte';
+	import { MetaTags } from 'svelte-meta-tags';
 </script>
 
 <svelte:head>
-	<meta name="description" content="thoughts, stories and ideas i guess" />
-	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:title" content="krido" />
-	<meta name="twitter:description" content="thoughts, stories and ideas i guess" />
-	<meta name="twitter:site" content="@yuxxeun" />
-	<meta name="twitter:creator" content="@yuxxeun" />
-	<meta
-		name="twitter:image"
-		content="https://raw.githubusercontent.com/yuxxeun/zea/main/image/gradient.jpg"
+	<MetaTags
+		title="yuxxeun"
+		description="thoughts, stories and very ideas"
+		canonical="https://www.yuxxeun.vercel.app/"
+		openGraph={{
+			url: 'https://www.yuxxeun.vercel.app',
+			title: 'yuxxeun',
+			description: 'thoughts, stories and very ideas',
+			images: [
+				{
+					url: 'https://raw.githubusercontent.com/yuxxeun/yuxxeun/main/header.png',
+					width: 800,
+					height: 600,
+					alt: 'Og Image Alt'
+				},
+				{
+					url: 'https://raw.githubusercontent.com/yuxxeun/yuxxeun/main/header.png',
+					width: 900,
+					height: 800,
+					alt: 'Og Image Alt Second'
+				},
+				{ url: 'https://raw.githubusercontent.com/yuxxeun/yuxxeun/main/header.png' },
+				{ url: 'https://raw.githubusercontent.com/yuxxeun/yuxxeun/main/header.png' }
+			],
+			site_name: 'yuxxeun'
+		}}
+		twitter={{
+			handle: '@yuxxeun',
+			site: 'https://yuxxeun.vercel.app',
+			cardType: 'summary_large_image',
+			title: 'yuxxeun',
+			description: 'thoughts, stories and very ideas',
+			image: 'https://raw.githubusercontent.com/yuxxeun/yuxxeun/main/header.png',
+			imageAlt: 'very cool image alt'
+		}}
+		facebook={{
+			appId: '666'
+		}}
 	/>
 	<link href="https://cdn.lineicons.com/3.0/lineicons.css" rel="stylesheet" />
-	<title>Krido</title>
+	<title>yuxxeun</title>
 </svelte:head>
 
 <div class="container">
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,10 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		fontFamily: {
+			basement: 'BasementGrotesque-Black, cursive',
+			space: 'SpaceMono-Regular, cursive'
+		},
 		extend: {
 			animation: {
 				'spin-slow': 'spin 5s linear infinite'
